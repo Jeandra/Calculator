e770e61c80c8cded7909cd61ab56409be85941a3
Jeandra <jeandra8789@gmail.com>
2023-04-27 21:34:38 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,22 @@ const clearBtn = document.querySelector(".clear");
 const delBtn = document.querySelector(".delete");
 const equalBtn = document.querySelector(".equal");
 let operation;
+
+function appendNumber(number) {
+    if (number === "." && currDisplay.innerHTML.includes(".")) return;
+    currDisplay.innerText += number;
+}
+
+function chooseOperation(operand) {
+    if (currDisplay.innerText === "") return;
+    getComputedStyle(operand);
+    operation = operand;
+    currDisplay.innerText += operand;
+    prevDisplay.innerText = currDisplay.innerText;
+    currDisplay.innerText = "";
+}
+
+function clearDisplay() {
+    currDisplay.innerText = "";
+    prevDisplay.innerText = "";
+}
