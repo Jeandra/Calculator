2296cd3903ad92c64d1f0507f02644880ce87388
Jeandra <jeandra8789@gmail.com>
2023-04-27 21:25:38 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
 // {}
+
+const currDisplay = document.querySelector(".curr-display");
+const prevDisplay = document.querySelector(".prev-display");
+const numbers = document.querySelectorAll(".number");
+const operands = document.querySelectorAll(".operation");
+const clearBtn = document.querySelector(".clear");
+const delBtn = document.querySelector(".delete");
+const equalBtn = document.querySelector(".equal");
+let operation;
